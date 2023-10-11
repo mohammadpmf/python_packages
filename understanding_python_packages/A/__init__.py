76d3f5316378e6783a21db18c01cc6817eb4adab
mohammadpmf <mohammad.pfallah@gmail.com>
2023-10-11 19:43:25 +0330

    V3 After adding jsbueno's answer

--- understanding_python_packages/A/__init__.py
+++ understanding_python_packages/A/__init__.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.insert(0, "C:/Users/Mohammad/Desktop/understanding_python_package_once_for_all/understanding_python_packages/A")
 from funcs import *
 
 if __name__ == "__main__":
