95164d91b4271d82c6fd95989e31fc8f5671c556
mohammadpmf <mohammad.pfallah@gmail.com>
2023-10-11 19:41:00 +0330

    V2 After jsbueno's answer

--- understanding_python_packages/B/main2.py
+++ understanding_python_packages/B/main2.py
@@ -1,4 +1,7 @@
-from ..A import *
+import sys
+sys.path.insert(0, "C:/Users/Mohammad/Desktop/understanding_python_package_once_for_all/understanding_python_packages")
+from A import funcs
 
 if __name__ == "__main__":
-    print("\n*** This file works fine alone :) ***\n")
+    funcs.f_a()
+    print("\n*** This file works fine alone :) ***\n")    
